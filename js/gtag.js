c4027065ce8952c31a0fe3b2bfb016fce65e8c05
Stephane-maker <contact@mydevproject.fr>
2021-01-28 19:03:24 +0100

    ajout du robot.txt et du sitemap.xml

---
+++ js/gtag.js
@@ -0,0 +1,9 @@
+//  Global site tag (gtag.js) - Google Analytics 
+async = "https://www.googletagmanager.com/gtag/js?id=G-HF0TN8JJLW";
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-HF0TN8JJLW');
